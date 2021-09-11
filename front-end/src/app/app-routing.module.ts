9ee84e956bb70139099fb3d15e47580d2c334762
Ajay Gomez <ajay.8640@gmail.com>
2021-09-11 00:15:51 -0500

    added routing for resource nav

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FamilyComponent } from './family/family.component';
 import { LoginComponent } from './login/login.component';
+import { ResourceDashboardComponent } from './resource-dashboard/resource-dashboard.component';
+import { ResourceSettingsComponent } from './resource-settings/resource-settings.component';
 import { ResourceComponent } from './resource/resource.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   { path: 'resource', component: ResourceComponent},
+  { path: 'resourceSettings', component: ResourceSettingsComponent},
+  { path: 'resourceDashboard', component: ResourceDashboardComponent},
   { path: 'family', component: FamilyComponent},
-
-
 ];
 
 @NgModule({
